282d7b6291d6c9e726777846ad050ad9e68de92a
Robert Soriano <sorianorobertc@gmail.com>
2022-06-29 21:20:17 -0700

    remove playground

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
-import { createStore, reconcile, Store, unwrap } from 'solid-js/store';
+import { createStore, reconcile, Store } from 'solid-js/store';
 import { onCleanup } from 'solid-js';
 import { subscribe, snapshot } from 'valtio/vanilla';
+import { klona } from 'klona/json';
 
 export function useSnapshot<T extends object>(proxyObject: T) {
-  const [state, setState] = createStore(unwrap(proxyObject));
+  const initialState = klona(proxyObject);
+  const [state, setState] = createStore(initialState);
 
   const close = subscribe(proxyObject, () => {
     const snap = snapshot(proxyObject);
-    setState(reconcile(snap as any));
+    const updatedState = klona(snap) as any
+    setState(reconcile(updatedState));
   });
 
   onCleanup(close);
