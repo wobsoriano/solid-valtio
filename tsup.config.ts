74266e6f97c6ca31f517986c9f7abcc823235c0d
Robert Soriano <sorianorobertc@gmail.com>
2022-07-02 10:17:00 -0700

    feat: export vanilla under solid-valtio/vanilla

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/index.ts', 'src/utils.ts'],
+  entry: ['src/index.ts', 'src/utils.ts', 'src/vanilla.ts'],
   clean: true,
   format: ['cjs', 'esm'],
   dts: true,
