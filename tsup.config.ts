faea4e2f3735df33eb211e3807fe18f5a4729f10
Robert Soriano <sorianorobertc@gmail.com>
2022-07-02 10:11:31 -0700

    feat: export utils under solid-valtio/utils

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: ['src/index.ts', 'src/utils.ts'],
   clean: true,
   format: ['cjs', 'esm'],
   dts: true,
